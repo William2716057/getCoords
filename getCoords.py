a18ff083d3a2913bfe492fece52bec301404bc44
User <Willspri@gmail.com>
2025-03-04 10:32:37 +1300

    started

---
+++ getCoords.py
@@ -0,0 +1,28 @@
+import requests
+from geopy.geocoders import Nominatim
+
+def get_coordinates(address):
+    geolocator = Nominatim(user_agent="geo_converter")
+    location = geolocator.geocode(address)
+    if location:
+        return location.latitude, location.longitude
+    else:
+        return None
+
+def main():
+    addresses = [
+        "1600 Amphitheatre Parkway, Mountain View, CA",
+        "221B Baker Street, London",
+        "Eiffel Tower, Paris"
+    ]
+    
+    for address in addresses:
+        coords = get_coordinates(address)
+        if coords:
+            print(f"{address}\nCoordinates: {coords}\n")
+        else:
+            print(f"{address}\nCoordinates: Not found\n")
+
+if __name__ == "__main__":
+    main()
+
